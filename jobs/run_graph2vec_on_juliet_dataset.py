d388c7e13ea8c5ca518af628baefac2791818d0a
xihajun <www.junfan.com@gmail.com>
2019-06-21 12:16:37 +0100

    Wait for whole dataset

--- jobs/run_graph2vec_on_juliet_dataset.py
+++ jobs/run_graph2vec_on_juliet_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 from code.preprocess_code import code2vec
 
 juliet = pd.read_csv("../data/juliet.csv.zip")
-code2vec(juliet)
+code2vec(juliet[0:10000])
 
 # will save output to ../data/graph_embeddings.csv
 
