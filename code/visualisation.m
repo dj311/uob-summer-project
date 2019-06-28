2ac184adf69dd5208516ff44d45a597f0227d728
xihajun <www.junfan.com@gmail.com>
2019-06-28 11:00:05 +0100

    visualisation data uploaded

--- code/visualisation.m
+++ code/visualisation.m
@@ -2,10 +2,22 @@
 label_str = readtable('/Users/xihajun/Documents/SummerProject/uob-summer-project/data/label.csv');
 label = readtable('/Users/xihajun/Documents/SummerProject/uob-summer-project/data/visualisation.csv');
 label = label{:,:};
-
+%label_s = char(label_str{:,2});
+label_s = label_str{:,2};
+true_label = []
+a = {'CWE-191'}
+b = {'CWE-191'}
+for i = 1:length(label_s)
+    if (isequal(label_s(i),a))||(isequal(label_s(i),b))
+        true_label(i) = 1;
+    else
+        true_label(i) = 0;
+    end
+end
+        
 % Plot scatters
 figure
-gscatter(label(:,1), label(:,2),label_str{:,2});
+gscatter(label(:,1), label(:,2),true_label);
 title("True label")
 
 figure
