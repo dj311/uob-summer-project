32abce3a128745767fc8ee259f5dd1032f550afb
Sydney Vertigan <sydverti@gmail.com>
2019-08-01 13:47:33 +0100

    #41 creating scripts for testing on progol 4.4

---
+++ code/ILP-progol-test-labels.pl
@@ -0,0 +1,17 @@
+
+%% learning task
+%% positive examples
+bug(bad_232103_id_168_l_27,bad_232103_id_146_l_32).
+bug(bad_232029_id_168_l_27,bad_232029_id_146_l_32).
+bug(bad_231979_id_171_l_25,bad_231979_id_135_l_33).
+bug(bad_62860_id_132_l_27,bad_62860_id_120_l_32).
+bug(bad_62805_id_135_l_27,bad_62805_id_111_l_35).
+
+%% negative examples
+:- bug(good_62805_id_207_l_34,good_62805_id_183_l_42).
+:- bug(good_62860_id_179_l_34,good_62860_id_166_l_39).
+:- bug(good_231979_id_195_l_27,good_231979_id_155_l_35).
+:- bug(good_232029_id_178_l_29,good_232029_id_151_l_34).
+:- bug(good_232103_id_178_l_29,good_232103_id_151_l_34).
+
+    
